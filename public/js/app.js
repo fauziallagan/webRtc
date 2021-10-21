125351ea38d0d45bc1164cf34a7b5d194a94360e
fauziallagan <fauziallagan@gmail.com>
2021-10-21 14:48:04 +0700

    change steps

--- public/js/app.js
+++ public/js/app.js
@@ -4,14 +4,14 @@ const configuration = {
   iceServers: [
     {
       urls: [
-        'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302',
-        'stun:stun3.l.google.com:19302',
-        'stun:stun4.l.google.com:19302',
-        'stun:stun.services.mozilla.com',
+        // 'stun:stun1.l.google.com:19302',
+        // 'stun:stun2.l.google.com:19302',
+        // 'stun:stun3.l.google.com:19302',
+        // 'stun:stun4.l.google.com:19302',
+        // 'stun:stun.services.mozilla.com',
         'stun:stun.fwdnet.net',
         'stun:stun.sipgate.net',
-        'stun:stun.'
+        // 'stun:stun.'
         
       ],
     },
